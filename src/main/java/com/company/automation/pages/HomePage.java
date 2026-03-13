7029457c6f45d9f7427bc0b14cf33e17ff550b34
Nguyen Khanh Duy <nguyenk@opentext.com>
2026-03-13 14:28:43 +0700

    feat(component): add header component

--- src/main/java/com/company/automation/pages/HomePage.java
+++ src/main/java/com/company/automation/pages/HomePage.java
@@ -2,11 +2,15 @@ package com.company.automation.pages;
 
 import com.company.automation.config.ConfigReader;
 import com.company.automation.core.BasePage;
+import com.company.automation.components.HeaderComponent;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
-    private final By logo = By.xpath("//img[@alt='Website for automation practice']");
+    private final By logo =
+            By.xpath("//img[@alt='Website for automation practice']");
+
+    private final HeaderComponent header = new HeaderComponent();
 
     public void open() {
         openUrl(ConfigReader.get("baseUrl"));
@@ -14,5 +18,9 @@ public class HomePage extends BasePage {
 
     public boolean isHomePageDisplayed() {
         return isDisplayed(logo);
+    }
+
+    public void goToLogin() {
+        header.clickLogin();
     }
 }
