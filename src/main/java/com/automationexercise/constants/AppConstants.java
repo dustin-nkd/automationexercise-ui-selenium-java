93358a493b10934d1b44e570eb0bb3b12d210ea4
Nguyen Khanh Duy <nguyenk@opentext.com>
2026-03-20 22:21:20 +0700

    feat: add core utility and configuration classes

---
+++ src/main/java/com/automationexercise/constants/AppConstants.java
@@ -0,0 +1,32 @@
+package com.automationexercise.constants;
+
+public class AppConstants {
+
+    private AppConstants() {}
+
+    // ===== URLS =====
+    public static final String HOME_URL        = "/";
+    public static final String LOGIN_URL       = "/login";
+    public static final String PRODUCT_URL     = "/products";
+    public static final String CART_URL        = "/view_cart";
+    public static final String TEST_CASES_URL  = "/test_cases";
+    public static final String API_TESTING_URL = "/api_list";
+
+    // ===== PAGES TITLES =====
+    public static final String HOME_TITLE       = "Automation Exercise";
+    public static final String LOGIN_TITLE      = "Automation Exercise = Signup / Login";
+    public static final String PRODUCTS_TITLE   = "Automation Exercise - All Products";
+    public static final String CART_TITLE       = "Automation Exercise - Checkout";
+    public static final String TEST_CASES_TITLE = "Automation Exercise - Test Cases";
+
+    // ===== MESSAGES =====
+    public static final String LOGIN_SUCCESS_MSG = "Logged in as";
+    public static final String REGISTER_SUCCESS_MSG = "ACCOUNT CREATED!";
+    public static final String DELETE_SUCCESS_MSG = "ACCOUNT DELETED!";
+    public static final String ORDER_SUCCESS_MSG = "ORDER PLACED!";
+    public static final String EMAIL_EXISTS_MSG = "Email address already exist!";
+    public static final String LOGIN_ERROR_MSG = "Your email or password is incorrect!";
+    public static final String CONTACT_SUCCESS_MSG = "Success! Your details have been submitted successfully!";
+    public static final String REVIEW_SUCCESS_MSG = "Thank you for your review.";
+    public static final String SUBSCRIPTION_SUCCESS_MSG = "You have been successfully subscribed!";
+}
---
+++ src/main/java/com/automationexercise/utils/DriverManager.java
@@ -0,0 +1,75 @@
+package com.automationexercise.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+
+public class DriverManager {
+
+    private static final ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    private DriverManager() {}
+
+    public static WebDriver getDriver() {
+        if (driverThread.get() == null) {
+            initDriver();
+        }
+        return driverThread.get();
+    }
+
+    private static void initDriver() {
+        String browser = ConfigReader.getBrowser().toLowerCase();
+        boolean headless = ConfigReader.isHeadless();
+        WebDriver driver;
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (headless) firefoxOptions.addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions= builChromeOptions(headless);
+                driver = new ChromeDriver(chromeOptions);
+                break;
+        }
+
+        configureDriver(driver);
+        driverThread.set(driver);
+    }
+
+    private static ChromeOptions builChromeOptions(boolean headless) {
+        ChromeOptions options = new ChromeOptions();
+        if (headless) options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-features=PasswordManagerOnBoarding");
+        return options;
+    }
+
+    private static void configureDriver(WebDriver driver) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getImplicitWait()));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(ConfigReader.getPageLoadTimeOut()));
+    }
+
+    public static void quitDriver() {
+        if (driverThread.get() != null) {
+            driverThread.get().quit();
+            driverThread.remove();
+        }
+    }
+}
