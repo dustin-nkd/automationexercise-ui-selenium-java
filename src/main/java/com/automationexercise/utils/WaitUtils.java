b64f4bb00ac53f614728f8867f9d6826488ed050
Nguyen Khanh Duy <nguyenk@opentext.com>
2026-03-23 21:51:13 +0700

    refacor: replace PageFactory with By locators for element identification

--- src/main/java/com/automationexercise/utils/WaitUtils.java
+++ src/main/java/com/automationexercise/utils/WaitUtils.java
@@ -13,30 +13,30 @@ public class WaitUtils {
     private WaitUtils() {}
 
     private static WebDriverWait getWait(WebDriver driver) {
-        return new WebDriverWait(driver, Duration.ofSeconds(ConfigReader.getImplicitWait()));
-    }
-
-    public static WebElement waitForVisible(WebDriver driver, WebElement element) {
-        return getWait(driver).until(ExpectedConditions.visibilityOf(element));
-    }
-
-    public static WebElement waitForClickable(WebDriver driver, WebElement element) {
-        return getWait(driver).until(ExpectedConditions.elementToBeClickable(element));
+        return new WebDriverWait(driver, Duration.ofSeconds(ConfigReader.getExplicitWait()));
     }
 
     public static WebElement waitForVisible(WebDriver driver, By locator) {
         return getWait(driver).until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    public static boolean waitForTextPresent(WebDriver driver, WebElement element, String text) {
-        return getWait(driver).until(ExpectedConditions.textToBePresentInElement(element, text));
+    public static WebElement waitForClickable(WebDriver driver, By locator) {
+        return getWait(driver).until(ExpectedConditions.elementToBeClickable(locator));
     }
 
-    public static boolean waitForUrlContains(WebDriver driver, WebElement element, String url) {
+    public static boolean waitForTextPresent(WebDriver driver, By locator, String text) {
+        return getWait(driver).until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+    }
+
+    public static boolean waitForUrlContains(WebDriver driver, String url) {
         return getWait(driver).until(ExpectedConditions.urlContains(url));
     }
 
     public static boolean waitForElementInvisible(WebDriver driver, By locator) {
         return getWait(driver).until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public static WebElement waitForPresent(WebDriver driver, By locator) {
+        return getWait(driver).until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 }
